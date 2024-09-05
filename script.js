b9b6b1d1891c656f582bc4af8659f99b8b6ffdc8
unknown <danielram2023@gmail.com>
2024-09-05 16:27:37 -0400

    added functionality for adding new tasks as list items, and some error handling

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const addTaskButton = document.getElementById("add-task");
 const taskInput = document.getElementById("new-task");
+const taskList = document.getElementById("task-list");
 
 addTaskButton.addEventListener("click", function() {
+    const task = taskInput.value; // get the task from input
+
+    const listItem = document.createElement("li"); // create a list item
+
+    listItem.textContent = task; // save the task in the list item
+
+    taskList.appendChild(listItem); // add the list item to the list
+
+    taskInput.value = ""; // clear the input box
+});
+
+// Event listener for using enter button to input information and error handling
+// for empty tasks
+taskInput.addEventListener("keydown", function(event) {
     const task = taskInput.value;
 
-    console.log(task);
-});
+    if (event.key === "Enter" && task !== "") {
+        addTaskButton.click();
+    }
+
+})
